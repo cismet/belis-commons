18d7537c08c29147ed8e74263e5848290b9474b0
JeanAtCismet <jean.ruiz@cismet.de>
2013-09-18 16:46:42 +0200

    some PropertyConstants added

---
+++ src/main/java/de/cismet/belis/commons/constants/ArbeitsprotokollPropertyConstants.java
@@ -0,0 +1,57 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+/*
+ *  Copyright (C) 2011 jruiz
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.cismet.belis.commons.constants;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   jruiz
+ * @version  $Revision$, $Date$
+ */
+public final class ArbeitsprotokollPropertyConstants extends PropertyConstants {
+
+    //~ Static fields/initializers ---------------------------------------------
+
+    public static final String PROP__FK_MAUERLASCHE = "fk_mauerlasche";
+    public static final String PROP__MATERIAL = "material";
+    public static final String PROP__FK_LEUCHTE = "fk_leuchte";
+    public static final String PROP__MONTEUR = "monteur";
+    public static final String PROP__FK_LEITUNG = "fk_leitung";
+    public static final String PROP__BEMERKUNG = "bemerkung";
+    public static final String PROP__DEFEKT = "defekt";
+    public static final String PROP__FK_STANDORT = "fk_standort";
+    public static final String PROP__FK_ABZWEIGDOSE = "fk_abzweigdose";
+    public static final String PROP__FK_SCHALTSTELLE = "fk_schaltstelle";
+    public static final String PROP__DATUM = "datum";
+    public static final String PROP__FK_STATUS = "fk_status";
+    public static final String PROP__FK_ARBEITSAUFTRAG = "fk_arbeitsauftrag";
+
+    //~ Constructors -----------------------------------------------------------
+
+    /**
+     * Creates a new KassenzeichenPropertyConstants object.
+     */
+    private ArbeitsprotokollPropertyConstants() {
+    }
+}
